e8a0c780bae9238e73fea43b42e95013f3b6550d
cpepin <cam@cpepin.com>
2019-11-07 04:45:13 -0500

    Updating template title

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Nutrition Tracker',
+      title: 'Squad Leader',
       template: './public/index.ejs',
       isCordova: process.env.CORDOVA,
     }),
