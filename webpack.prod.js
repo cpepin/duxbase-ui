5c75b375a28dd92ceeb48730067567e7261efe4a
cpepin <cam@cpepin.com>
2019-11-26 16:58:57 -0500

    Disabling bundle analyzer in production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,13 @@
 const merge = require('webpack-merge');
 const CompressionPlugin = require('compression-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [new CompressionPlugin(), new BundleAnalyzerPlugin()],
+  plugins: [new CompressionPlugin() /* , new BundleAnalyzerPlugin() */],
   optimization: {
     splitChunks: {
       chunks: 'all',
