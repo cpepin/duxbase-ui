7f3093d3ba420ea6f4af4e54fd939087bf81b614
cpepin <cam@cpepin.com>
2019-11-27 10:45:53 -0500

    Refactoring TeamList to be comprised of individual Team components

---
+++ src/pages/Teams/components/Team/Team.js
@@ -0,0 +1,33 @@
+import { h } from 'preact';
+import { memo, useMemo } from 'preact/compat';
+import { Link } from '@reach/router';
+
+import TeamIcon from 'components/Icons/Team';
+import ArrowRightCircleIcon from 'components/Icons/ArrowRightCircle';
+
+import './index.scss';
+
+function Team({ team }) {
+  const teamUrl = useMemo(() => `/teams/${team.id}`, [team]);
+
+  return (
+    <li class="team-list__team">
+      <Link to={teamUrl}>
+        {/* <NotificationContainer class="team-list__item__icon"> */}
+        <TeamIcon class="team-list__team__icon" />
+        {/* </NotificationContainer> */}
+        <div class="team-list__team__description">
+          <div>
+            <span class="team-list__team__description__title">{team.name}</span>
+            {/* <span class="team-list__item__description__event team-list__item__description__event--urgent">
+              Event tomorrow
+            </span> */}
+          </div>
+          <ArrowRightCircleIcon class="team-list__team__description__arrow" />
+        </div>
+      </Link>
+    </li>
+  );
+}
+
+export default memo(Team);
--- src/pages/Teams/components/TeamList/TeamList.js
+++ src/pages/Teams/components/TeamList/TeamList.js
@@ -1,56 +1,22 @@
 import { h } from 'preact';
 import { memo } from 'preact/compat';
-import { Link } from '@reach/router';
 
-import TeamIcon from 'components/Icons/Team';
-import ArrowRightCircleIcon from 'components/Icons/ArrowRightCircle';
-import NotificationContainer from 'components/NotificationContainer';
+import Team from 'pages/Teams/components/Team';
 
 import './index.scss';
+
+const teams = [
+  { name: 'The Squad', id: '1' },
+  { name: 'Bloom Weddings', id: '2' },
+  { name: 'Heberts Restaurant', id: '3' },
+];
 
 function TeamList() {
   return (
     <ul class="team-list">
-      <li class="team-list__item">
-        <Link to="/home">
-          <NotificationContainer class="team-list__item__icon">
-            <TeamIcon />
-          </NotificationContainer>
-          <div class="team-list__item__description">
-            <div>
-              <span class="team-list__item__description__title">The Squad</span>
-              <span class="team-list__item__description__event team-list__item__description__event--urgent">
-                Event tomorrow
-              </span>
-            </div>
-            <ArrowRightCircleIcon class="team-list__item__description__arrow" />
-          </div>
-        </Link>
-      </li>
-      <li class="team-list__item">
-        <Link to="/home">
-          <TeamIcon class="team-list__item__icon" />
-          <div class="team-list__item__description">
-            <div>
-              <span class="team-list__item__description__title">Bloom Weddings</span>
-              <span class="team-list__item__description__event">Event next week</span>
-            </div>
-            <ArrowRightCircleIcon class="team-list__item__description__arrow" />
-          </div>
-        </Link>
-      </li>
-      <li class="team-list__item">
-        <Link to="/home">
-          <TeamIcon class="team-list__item__icon" />
-          <div class="team-list__item__description">
-            <div>
-              <span class="team-list__item__description__title">Heberts Restaurant</span>
-              <span class="team-list__item__description__event">No events scheduled</span>
-            </div>
-            <ArrowRightCircleIcon class="team-list__item__description__arrow" />
-          </div>
-        </Link>
-      </li>
+      {teams.map(team => (
+        <Team team={team} />
+      ))}
     </ul>
   );
 }
