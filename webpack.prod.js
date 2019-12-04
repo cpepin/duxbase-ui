ecb1664e1253fdf22561a3e194a7a5c5f6b8fc13
cpepin <cam@cpepin.com>
2019-12-04 18:16:30 -0500

    Adding netlify redirects to support browser routing

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,20 @@
 const merge = require('webpack-merge');
 const CompressionPlugin = require('compression-webpack-plugin');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const common = require('./webpack.common');
+
+const copiedFiles = process.env.CORDOVA ? ['public/initCordova.js'] : ['public/_redirects'];
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [new CompressionPlugin() /* , new BundleAnalyzerPlugin() */],
+  plugins: [
+    new CompressionPlugin(),
+    new CopyPlugin(copiedFiles),
+    /* , new BundleAnalyzerPlugin() */
+  ],
   optimization: {
     splitChunks: {
       chunks: 'all',
